5d378ff04d448075a272f21e671f9071de75133c
sautkrista <sautkrista94@gmail.com>
2021-03-31 10:00:10 +0700

    login logout halaman admin OK

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['prefix'=>'admin', 'middleware'=>'auth'], function() {
+    Route::get('/', 'Admin\DashboardController@index');
+});
+
